218af7ed70366b84bb7a3d8502601e4c60b1833b
Mohd Mustafa Abdul Subhan Abdul subhan <mustafa20697@gmail.com>
2025-08-17 15:50:51 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "linux-rg"
+  location = "East US"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "linux-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
